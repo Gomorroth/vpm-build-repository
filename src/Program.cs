e45e624d2a9289f689182a55d37d7b3458616f71
Ashley Scarlet <70315656+AshleyScarlet@users.noreply.github.com>
2023-12-13 00:07:52 +0900

    SHA256（仮）

--- src/Program.cs
+++ src/Program.cs
@@ -1,8 +1,9 @@
-﻿using System.Buffers;
+using System.Buffers;
 using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
+using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -356,3 +357,52 @@ internal class PackageInfoZipUrl
 [JsonSerializable(typeof(Dictionary<string, PackageInfoZipUrl[]>))]
 [JsonSerializable(typeof(RepositoryPackageInfo))]
 internal sealed partial class SerializeContexts : JsonSerializerContext;
+internal static partial class Sha256
+{
+    private static ReadOnlySpan<ulong> K => 
+    [
+        0x428a2f98UL, 0x71374491UL, 0xb5c0fbcfUL, 0xe9b5dba5UL,
+        0x3956c25bUL, 0x59f111f1UL, 0x923f82a4UL, 0xab1c5ed5UL,
+        0xd807aa98UL, 0x12835b01UL, 0x243185beUL, 0x550c7dc3UL,
+        0x72be5d74UL, 0x80deb1feUL, 0x9bdc06a7UL, 0xc19bf174UL,
+        0xe49b69c1UL, 0xefbe4786UL, 0x0fc19dc6UL, 0x240ca1ccUL,
+        0x2de92c6fUL, 0x4a7484aaUL, 0x5cb0a9dcUL, 0x76f988daUL,
+        0x983e5152UL, 0xa831c66dUL, 0xb00327c8UL, 0xbf597fc7UL,
+        0xc6e00bf3UL, 0xd5a79147UL, 0x06ca6351UL, 0x14292967UL,
+        0x27b70a85UL, 0x2e1b2138UL, 0x4d2c6dfcUL, 0x53380d13UL,
+        0x650a7354UL, 0x766a0abbUL, 0x81c2c92eUL, 0x92722c85UL,
+        0xa2bfe8a1UL, 0xa81a664bUL, 0xc24b8b70UL, 0xc76c51a3UL,
+        0xd192e819UL, 0xd6990624UL, 0xf40e3585UL, 0x106aa070UL,
+        0x19a4c116UL, 0x1e376c08UL, 0x2748774cUL, 0x34b0bcb5UL,
+        0x391c0cb3UL, 0x4ed8aa4aUL, 0x5b9cca4fUL, 0x682e6ff3UL,
+        0x748f82eeUL, 0x78a5636fUL, 0x84c87814UL, 0x8cc70208UL,
+        0x90befffaUL, 0xa4506cebUL, 0xbef9a3f7UL, 0xc67178f2UL
+    ];
+
+    private static ReadOnlySpan<uint> H =>
+    [
+        0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
+    ];
+
+}
+
+// Utilities
+static partial class Sha256
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static uint Rot(this uint v, int n) => BitOperations.RotateLeft(v, n);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static uint Shr(this uint v, int n) => v >> n;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static uint Sigma0(this uint x) => x.Rot(30) ^ x.Rot(19) ^ x.Rot(10);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static uint Sigma1(this uint x) => x.Rot(26) ^ x.Rot(21) ^ x.Rot(7);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static uint sigma0(this uint x) => x.Rot(25) ^ x.Rot(14) ^ x.Rot(3);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static uint sigma1(this uint x) => x.Rot(15) ^ x.Rot(13) ^ x.Rot(10);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static uint Ch(this uint x, uint y, uint z) => (x & y) ^ (~x & z);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static uint Maj(this uint x, uint y, uint z) => (x & y) ^ (y & z) ^ (x & z);
+}
